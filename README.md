a7c37c80e5699829cb6558dc02cf106e1bdb5d18
jmargielewski <kuba.margielewski@gmail.com>
2020-10-25 10:07:46 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ethereum-smart-contract
+solc - solidity compiler
+mocha - JS test
+gonache-cli - test local ethereum network that we're going to deploy out contract to
+web3 - portal to the Ethereum world. Retrieve information form the network and make changes as well.
+HDWalletProvider - connect to some target network and unlock an account for use on that network
---
+++ deploy.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+const { MNEMONIC, INFURA_NODE } = require('./secrets');
+
+const provider = new HDWalletProvider(MNEMONIC, INFURA_NODE);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('accounts', accounts);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Deployed to:', result.options.address);
+};
+
+deploy();
