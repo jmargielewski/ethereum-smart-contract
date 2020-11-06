5b45b499d27af91029e76cc6679c7fb0019f8006
jmargielewski <kuba.margielewski@gmail.com>
2020-11-06 19:12:35 +0000

    deploy script

---
+++ Campaign/ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const { MNEMONIC, INFURA_NODE } = require('../../secrets');
+
+const provider = new HDWalletProvider(MNEMONIC, INFURA_NODE);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('accounts', accounts);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Deployed to:', result.options.address);
+};
+
+deploy();
