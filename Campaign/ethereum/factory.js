4633be867b1a9ed73582a5af6fabf4989c07adcd
jmargielewski <kuba.margielewski@gmail.com>
2020-11-09 21:50:49 +0000

    update

--- Campaign/ethereum/factory.js
+++ Campaign/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x6310C933BE3F1122642cC5f1e38fAbdAAA451F6B'
+  '0xa29d346c124e65Eb8097DCBe31CFf076ef906eD1'
 );
 
 export default instance;
